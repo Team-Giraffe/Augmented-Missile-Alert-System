5889cdeacf5c3557089fb87a632dd90c3f6ea980
Sae Hyun Song <17700563+saehyuns@users.noreply.github.com>
2018-04-24 22:48:30 -1000

    Changed to just email

--- confirmationPages/send.php
+++ confirmationPages/send.php
@@ -1,7 +1,7 @@
 <?php
-//Format "+x(xxx)xxx-xxxx@carrier-name
-$to = "8087229842@tmomail.net";
-$from = "saehyunsong123@gmail.com";
+//Format "+x(xxx)xxx-xxxx@carrier-name T-Mobile: tmomail.net ; AT&T: txt.att.net ; Verizon Wireless: vtext.com
+$to = "8083893588@vtext.com";
+$from = "SaeHyunSong@Saes-MBP.localdomain";
 $subject = "Test Message";
 $message = "Test text message!\n";
 $headers = "From: $from\r\n";
@@ -14,12 +14,12 @@ if (mail($to, $subject, $message, $headers)) {
 } else {
     echo "The text has failed!";
 }
-$to = "akirav@hawaii.edu";
+$to = "saehyuns@hawaii.edu";
 $message = "Test email message!\n";
 if (mail($to, $subject, $message, $headers)) {
     echo "The email has been sent!";
 } else {
     echo "The email has failed!";
 }
-header('Location: ../confirmationPages/success.html');
+//header('Location: ../confirmationPages/success.html');
 ?>
